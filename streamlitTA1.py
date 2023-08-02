9e757d7f69374994bc9fdbf6766b70b3fa332816
Malcolm Tavaria <max2k06@gmail.com>
2023-08-02 21:28:25 +0530

    code updated with radio functionality for negative and positive points

--- streamlitTA1.py
+++ streamlitTA1.py
@@ -43,10 +43,17 @@ mask_selecter= st.selectbox(
     "Choose mask",
     ["mask", "flask", "dusk"]
 )
-point_selecter= st.radio(
-    "Choose point type",
-    ["Positive", "Negative"]
-)
+st.session_state['point_prompt']= str(st.radio(
+    "Choose point type",["Positive", "Negative"]))
+
+
+# point_selecter= st.radio(
+#    "Choose point type",["Positive", "Negative"])
+# if point_selecter== "Positive":
+#     #how do i call function because without that the variables are not defined
+#     x=5
+
+
 add_mask_button= st.button(
     "Add Mask"
 )
@@ -63,20 +70,22 @@ st.button(
 
 if 'video_info' in st.session_state['stvideo_state'] :
     st.write(st.session_state['stvideo_state']["video_info"])
-    fig = px.imshow(st.session_state['stvideo_state']["origin_images"][0])
+    if 'fig' not in st.session_state:
+        st.session_state['fig']= px.imshow(st.session_state['stvideo_state']["origin_images"][0])
     if 'selected_points' not in st.session_state:
         st.session_state['selected_points']= []
-    st.session_state['selected_points'].append(plotly_events(fig).pop())
-    st.write(st.session_state['selected_points'])
+    try:
+        st.session_state['selected_points'].append(plotly_events(st.session_state['fig']).pop())
+        st.write(st.session_state['selected_points'])
+    except IndexError:
+        pass
    
-if 'point_prompt' not in st.session_state:
-    st.session_state['point_prompt'] = "Positive"
-
 if 'interactive_state' not in st.session_state:
     st.session_state['interactive_state']= {"positive_click_times":0, "negative_click_times":0}
 
 if 'click_state' not in st.session_state:
     st.session_state['click_state']= [[], []]
+
 
 if 'selected_points' in st.session_state:
     for i in range(len(st.session_state['selected_points'])):
@@ -84,11 +93,7 @@ if 'selected_points' in st.session_state:
         y_coordinate= st.session_state['selected_points'][i]['y']
         evt= (x_coordinate, y_coordinate)
         painted_image, video_state, interactive_state, operation_log= sam_refine(st.session_state['model'], st.session_state['stvideo_state'], st.session_state['point_prompt'],  st.session_state['interactive_state'], evt, st.session_state['click_state'])
-        st.image(painted_image)
-
-#wrap line 93 and 94 in a loop and line 71 72 73 can be placed under line 92 and should be part of the loop because same if condition
-# replace zero index of selected points with the variable that is being changed by the loop, "i".
-# look into ennumerate function of python, will help create a loop with a dynamic index called "i", which will be used instead of 0 in selected points.
-
+        st.session_state['fig'] = px.imshow(painted_image)
+        
 #Look at streamlit documentation, to add functionality of changing from positive to negative at user's click
 #Overwrite the plotly figure with the plotly of the painted image, replace line 66 with session state figure and blah
